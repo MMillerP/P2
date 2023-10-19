07c856cd334f376af8bb5207d2db30a3f9bfb6f3
apmiller111@gmail.com <Bellum223&G4>
2023-10-18 21:16:08 -0400

    Fixed Program unable to find main

--- src/main/java/com/CustomerDatabase/Main.java
+++ src/main/java/com/CustomerDatabase/Main.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class P2Application {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(P2Application.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
